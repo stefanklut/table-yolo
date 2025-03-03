2ee35b774ec87adb5a48c13356dceabdaa83a746
Stefan Klut <stefanklut12@gmail.com>
2025-03-03 14:48:39 +0100

    Combine the two training scripts to ensure that fixes for one are fixes for both

--- train.py
+++ train.py
@@ -77,19 +77,30 @@ def add_cfg_arguments(parser: argparse.ArgumentParser):
 
 def get_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Convert PubTabNet to YOLO format")
-    parser.add_argument("--pubtabnet_path", type=str, required=True, help="Path to PubTabNet JSONL file")
+    parser.add_argument("--pubtabnet_path", type=str, help="Path to PubTabNet JSONL file")
+    parser.add_argument("--publaynet_path", type=str, help="Path to PubTabNet JSONL file")
     parser.add_argument("--yolo_base", type=str, default="yolo11n.pt", help="YOLO base model")
 
     add_cfg_arguments(parser)
 
     args = parser.parse_args()
+
+    if args.pubtabnet_path is None and args.publaynet_path is None:
+        raise argparse.ArgumentError(args.pubtabnet_path, "Either --pubtabnet_path or --publaynet_path must be provided")
+    if args.pubtabnet_path is not None and args.publaynet_path is not None:
+        raise argparse.ArgumentError(args.pubtabnet_path, "Only one of --pubtabnet_path or --publaynet_path can be provided")
     return args
 
 
 def main(args: argparse.Namespace):
+
     with OptionalTemporaryDirectory() as tmp_dir:
-        pubtabnet_to_yolo = PubTabNetToYOLO(args.pubtabnet_path, tmp_dir)
-        pubtabnet_to_yolo.convert()
+        if args.pubtabnet_path is not None:
+            pubtabnet_to_yolo = PubTabNetToYOLO(args.pubtabnet_path, tmp_dir)
+            pubtabnet_to_yolo.convert()
+        else:
+            publaynet_to_yolo = PubTabNetToYOLO(args.publaynet_path, tmp_dir)
+            publaynet_to_yolo.convert()
 
         yolo_data_path = Path(tmp_dir).joinpath("yolo.yaml")
 
@@ -98,6 +109,7 @@ def main(args: argparse.Namespace):
         kwargs = vars(args)
         # Remove non-YOLO arguments
         kwargs.pop("pubtabnet_path")
+        kwargs.pop("publaynet_path")
         kwargs.pop("yolo_base")
 
         kwargs["data"] = str(yolo_data_path)
