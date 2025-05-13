7e1223ba61bdf7c2012e5eb7c1885c8f4041b184
Stefan Klut <stefanklut12@gmail.com>
2025-05-13 10:17:20 +0200

    Add visualization script for YOLO model predictions with argument parsing and result handling

---
+++ tooling/visualize_yolo.py
@@ -0,0 +1,52 @@
+import argparse
+import sys
+from pathlib import Path
+
+from ultralytics import YOLO
+
+sys.path.append(str(Path(__file__).resolve().parent.joinpath("..")))
+from utils.input_utils import SUPPORTED_IMAGE_FORMATS, get_file_paths
+
+
+def get_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Visualization of prediction/GT of model")
+
+    yolo_args = parser.add_argument_group("YOLO")
+    yolo_args.add_argument("--yolo_model", type=str, required=True, help="Path to YOLO model")
+
+    io_args = parser.add_argument_group("IO")
+    # io_args.add_argument("-t", "--train", help="Train input folder/file",
+    #                         nargs="+", action="extend", type=str, default=None)
+    io_args.add_argument("-i", "--input", help="Input folder/file", nargs="+", action="extend", type=str, default=None)
+    io_args.add_argument("-o", "--output", help="Output folder", type=str)
+
+    parser.add_argument("--save", nargs="?", const="all", default=None, help="Save images instead of displaying")
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main(args):
+    # Load a model
+    model = YOLO(args.yolo_model)
+
+    # Run batched inference on a list of images
+    input_paths = get_file_paths(args.input, SUPPORTED_IMAGE_FORMATS)
+
+    results = model(input_paths)
+
+    # Process results list
+    for result in results:
+        boxes = result.boxes  # Boxes object for bounding box outputs
+        masks = result.masks  # Masks object for segmentation masks outputs
+        keypoints = result.keypoints  # Keypoints object for pose outputs
+        probs = result.probs  # Probs object for classification outputs
+        obb = result.obb  # Oriented boxes object for OBB outputs
+        result.show()  # display to screen
+        # result.save(filename="result.jpg")  # save to disk
+
+
+if __name__ == "__main__":
+    args = get_arguments()
+    main(args)
